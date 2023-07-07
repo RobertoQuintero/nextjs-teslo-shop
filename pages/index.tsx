1594bd0fa3fffdabc3f1cc1e62fead283d2a6fb7
Roberto Quintero <langdonhp435@gmail.com>
2023-07-07 15:33:17 -0600

    Crea ShopLayout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import { ShopLayout } from "@/components/layouts"
+import { Typography } from "@mui/material"
 
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <ShopLayout  title={"Teslo-Shop - Home"} pageDescription={"Encuentra los mejores productos de Teslo aqui"}>
+      <Typography variant="h1" component='h1'>Tienda</Typography>
+      <Typography variant="h2" sx={{mb:1}}>Todos los productos</Typography>
+    </ShopLayout>
   )
 }
 
