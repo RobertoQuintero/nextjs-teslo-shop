75c55d8ec6a4ed8b77d48d98425ebb8be42feb35
Roberto Quintero <langdonhp435@gmail.com>
2023-07-08 17:11:44 -0600

    Temina diseño de pagia carrito de compras

---
+++ components/cart/CartList.tsx
@@ -0,0 +1,50 @@
+import NextLink from 'next/link'
+import { initialData } from "@/database/products"
+import { Box, Button, CardActionArea, CardMedia, Grid, Link, Typography } from "@mui/material"
+import { ItemCounter } from '../ui'
+
+const productsInCart=[
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+]
+export const CartList = () => {
+  return (
+    <>
+      {
+        productsInCart.map(product=>(
+          <Grid container spacing={2} key={product.slug} sx={{mb:1}}>
+            <Grid item xs={3}>
+              {/* llevar a la pagina del producto */}
+              <NextLink href='/product/slug' passHref legacyBehavior>
+                <Link>
+                  <CardActionArea>
+                    <CardMedia 
+                      image={`products/${product.images[0]}`}
+                      component='img'
+                      sx={{borderRadius:'5px'}}
+                    />
+                  </CardActionArea>
+                </Link>
+              </NextLink>
+            </Grid>
+            <Grid item xs={7}>
+              <Box display='flex' flexDirection='column'>
+                <Typography variant='body1'>{product.title}</Typography>
+                <Typography variant='body1'>Talla <strong>M</strong></Typography>
+                <ItemCounter/>
+              </Box>
+            </Grid>
+            <Grid item xs={2} display='flex' alignItems='center' flexDirection='column'>
+              <Typography variant='subtitle1'>${product.price}</Typography>
+
+              <Button variant='text' color='secondary'>
+                Remover
+              </Button>
+            </Grid>
+          </Grid>
+        ))
+      }
+    </>
+  )
+}
