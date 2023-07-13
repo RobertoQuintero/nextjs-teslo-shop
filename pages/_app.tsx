5ea05156508cbc0342c5480118b27f5a2122e1ae
Roberto Quintero <langdonhp435@gmail.com>
2023-07-13 11:49:20 -0600

    Agrega SWR hook de manera global y realiza peticion a products en HomePage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,21 @@ import '@/styles/globals.css'
 import { lightTheme } from '@/themes'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
+import { SWRConfig } from 'swr'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <SWRConfig 
+      value={{
+        // refreshInterval: 3000,
+        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+      }}
+    >
     <ThemeProvider theme={lightTheme}>
       <CssBaseline/>
       <Component {...pageProps} />
     </ThemeProvider>
+      
+    </SWRConfig>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,27 @@
 import { ShopLayout } from "@/components/layouts"
 import { ProductList } from "@/components/products"
-import { initialData } from "@/database/products"
+import { useProducts } from "@/hooks"
 import {  Typography } from "@mui/material"
 
-const Home = () => {
+
+const HomePage = () => {
+  const {products,isLoading}= useProducts('/products')
+
+
   return (
     <ShopLayout  title={"Teslo-Shop - Home"} pageDescription={"Encuentra los mejores productos de Teslo aqui"}>
       <Typography variant="h1" component='h1'>Tienda</Typography>
       <Typography variant="h2" sx={{mb:1}}>Todos los productos</Typography>
 
-      <ProductList
-        products={initialData.products as any}
-      />
+      {
+        isLoading
+          ? <h1>Cargando...</h1>
+          : <ProductList
+              products={products}
+            />
+      }
     </ShopLayout>
   )
 }
 
-export default Home
+export default HomePage
